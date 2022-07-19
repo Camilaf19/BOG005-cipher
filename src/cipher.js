1ed293b4077c65d0f8162d75e5963cf2171bee84
Camilaf19 <cam.fonseca19@gmail.com>
2022-07-19 13:05:47 -0500

    CSS y código cipher.encode finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,24 @@
+
 const cipher = {
-  // ...
+    encode: function (offset, string) {
+        let mistring = "";
+        for (let i = 0; i < string.length; i++) {
+            let capitalLetters = string.toUpperCase();
+            let ascciNum = capitalLetters[i].charCodeAt(0);
+            let position = (ascciNum - 65 + offset) % 26 + 65;
+            let resultCipher = String.fromCharCode(position);
+            mistring = mistring + resultCipher
+        }
+        return mistring
+
+
+    },
+
+    decode: function (offset, string) {
+
+    }
 };
+
+console.log(cipher)
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const selectPassword= document.getElementById ("password1")
+selectPassword.addEventListener ("click", fnSelect)
+function fnSelect (event) {
+    event.preventDefault ()
+    const clueOffset = selectPassword.value };
+
+    const btCifrar = document.getElementById("btCifrar")
+btCifrar.addEventListener ("click", fnCifrar)
+function fnCifrar (event) {
+    event.preventDefault ()
+    const clueString= document.getElementById("Clue").value 
+
+const clueOffset = parseInt(selectPassword.value) 
+
+
+console.log (clueOffset, clueString)
+
+
+document.getElementById("result").value = cipher.encode(clueOffset,clueString); 
+};
+
