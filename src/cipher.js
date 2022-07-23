2c1903268a399f71310a1c6f6317e22f2444ee6b
Camilaf19 <cam.fonseca19@gmail.com>
2022-07-23 18:24:05 -0500

    Cifrado y descifrado de mayusculas. 100% de los test unitarios cubiertos y passed!

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,54 @@
 
 const cipher = {
     encode: function (offset, string) {
-        let mistring = "";
+        if (offset === undefined && string === undefined) {
+            throw new TypeError;
+        } else if (offset === 0 && string === 0) {
+            throw new TypeError;
+        } else if (offset === null || string === []) {
+            throw new TypeError;
+        }
+
+        let myString = "";
         for (let i = 0; i < string.length; i++) {
             let capitalLetters = string.toUpperCase();
             let ascciNum = capitalLetters[i].charCodeAt(0);
             let position = (ascciNum - 65 + offset) % 26 + 65;
-            let resultCipher = String.fromCharCode(position);
-            mistring = mistring + resultCipher
+            myString = myString + String.fromCharCode(position);
         }
-        return mistring
-
+        return myString
 
     },
 
     decode: function (offset, string) {
+        if (offset === undefined && string === undefined) {
+            throw new TypeError;
+        } else if (offset === 0 && string === 0) {
+            throw new TypeError;
+        } else if (offset === null || string === []) {
+            throw new TypeError;
+        }
 
+        let mystring2 = ""
+        for (let i = 0; i < string.length; i++) {
+            let capitalLetters2 = string.toUpperCase();
+            let ascciNum2 = capitalLetters2[i].charCodeAt();
+
+            if (ascciNum2 >= 65 && ascciNum2 <= 90) {
+                let positionDecode = (ascciNum2 - 65 - offset) % 26;
+
+                if (positionDecode < 0) {
+                    positionDecode = positionDecode + 65 + 26;
+                    mystring2 = mystring2 + String.fromCharCode(positionDecode);
+                } else {
+                    positionDecode = positionDecode + 65;
+                    mystring2 = mystring2 + String.fromCharCode(positionDecode);
+                }
+            }
+        }
+
+        return mystring2
     }
 };
-
-console.log(cipher)
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//Cambio de pagina de bienvenida
+const welcomePage = document.getElementById ("Start")
+if (welcomePage) {
+welcomePage.addEventListener("click", changePage) }
 
-const selectPassword= document.getElementById ("password1")
-selectPassword.addEventListener ("click", fnSelect)
-function fnSelect (event) {
-    event.preventDefault ()
-    const clueOffset = selectPassword.value };
+function changePage () {
+location.href = "pagina.html";
+}
 
-    const btCifrar = document.getElementById("btCifrar")
-btCifrar.addEventListener ("click", fnCifrar)
-function fnCifrar (event) {
-    event.preventDefault ()
-    const clueString= document.getElementById("Clue").value 
+//Funciones de cifrado y descifrar 
+const selectPassword = document.getElementById("password1")
+selectPassword.addEventListener("click", fnSelect)
+function fnSelect(event) {
+    event.preventDefault()
 
-const clueOffset = parseInt(selectPassword.value) 
+}
 
+const btCifrar = document.getElementById("btCifrar")
+btCifrar.addEventListener("click", fnCifrar)
+function fnCifrar(event) {
+    event.preventDefault()
+    const clueString = document.getElementById("Clue").value
+    const clueOffset = parseInt(selectPassword.value)
 
-console.log (clueOffset, clueString)
+    document.getElementById("result").value = cipher.encode(clueOffset, clueString);
+}
 
+const passwordDecode = document.getElementById("password2")
+passwordDecode.addEventListener("click", fnSelect2)
+function fnSelect2(event) {
+    event.preventDefault()
+   
+}
 
-document.getElementById("result").value = cipher.encode(clueOffset,clueString); 
-};
+const btDescifrar = document.getElementById("btDescifrar")
+btDescifrar.addEventListener("click", fnDescifrar)
+function fnDescifrar(event) {
+    event.preventDefault()
+    const decodeString = document.getElementById("Clue2").value
+    const decodeOffset = parseInt(passwordDecode.value)
 
+    document.getElementById("result2").value= cipher.decode(decodeOffset, decodeString);
+}
