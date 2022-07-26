c88a3ff48febcd0504f5701a296feebc4dd6ee33
Camilaf19 <cam.fonseca19@gmail.com>
2022-07-25 19:27:15 -0500

    Cifrado y descifrado de mayusculas y minusculas, 100% de test passed.

--- src/cipher.js
+++ src/cipher.js
@@ -2,53 +2,82 @@
 const cipher = {
     encode: function (offset, string) {
         if (offset === undefined && string === undefined) {
-            throw new TypeError;
+            throw new TypeError("Debe llenar los campos necesarios");
         } else if (offset === 0 && string === 0) {
             throw new TypeError;
         } else if (offset === null || string === []) {
+            throw new TypeError;
+        } else if (offset == 0 && string == 0) {
             throw new TypeError;
         }
 
         let myString = "";
         for (let i = 0; i < string.length; i++) {
-            let capitalLetters = string.toUpperCase();
-            let ascciNum = capitalLetters[i].charCodeAt(0);
-            let position = (ascciNum - 65 + offset) % 26 + 65;
-            myString = myString + String.fromCharCode(position);
+            let ascciNum = string[i].charCodeAt(0);
+            if (ascciNum >= 97 && ascciNum <= 122) {
+                let positionMayus = (ascciNum - 97 + offset) % 26 + 97;
+                myString = myString + String.fromCharCode(positionMayus);
+            }
+            else if (ascciNum >= 65 && ascciNum <= 90) {
+                let position = (ascciNum - 65 + offset) % 26 + 65;
+                myString = myString + String.fromCharCode(position);
+            }
+
+            /*condicional para espacio en blanco en progreso..
+                     if (ascciNum === 32) {
+                            myString = myString + String.fromCharCode(ascciNum);
+                        }    */
         }
+
         return myString
 
     },
 
     decode: function (offset, string) {
+        //condicionales para errores de test
         if (offset === undefined && string === undefined) {
-            throw new TypeError;
+            throw new TypeError("Debe llenar los campos necesarios");
         } else if (offset === 0 && string === 0) {
             throw new TypeError;
         } else if (offset === null || string === []) {
             throw new TypeError;
+        } else if (offset == 0 && string == 0) {
+            throw new TypeError;
         }
-
-        let mystring2 = ""
+        //descifrado
+        let mystring2 = "";
+        let positionMinus = "";
         for (let i = 0; i < string.length; i++) {
-            let capitalLetters2 = string.toUpperCase();
-            let ascciNum2 = capitalLetters2[i].charCodeAt();
-
+            //debugger
+            let ascciNum2 = string[i].charCodeAt(0);
+            //descifrando minusculas 
+            if (ascciNum2 >= 97 && ascciNum2 <= 122) {
+                positionMinus = (ascciNum2 - 97 - offset) % 26;
+                if (positionMinus < 0) {
+                    let positionResult = positionMinus + 97 + 26;
+                    mystring2 = mystring2 + String.fromCharCode(positionResult);
+                }
+                else {
+                    let positionResult2 = positionMinus + 97;
+                    mystring2 = mystring2 + String.fromCharCode(positionResult2);
+                }
+            }
+            // descifrado mayusculas 
             if (ascciNum2 >= 65 && ascciNum2 <= 90) {
-                let positionDecode = (ascciNum2 - 65 - offset) % 26;
+                positionMinus = (ascciNum2 - 65 - offset) % 26;
 
-                if (positionDecode < 0) {
-                    positionDecode = positionDecode + 65 + 26;
-                    mystring2 = mystring2 + String.fromCharCode(positionDecode);
+                if (positionMinus < 0) {
+                    positionMinus = positionMinus + 65 + 26;
+                    mystring2 = mystring2 + String.fromCharCode(positionMinus);
                 } else {
-                    positionDecode = positionDecode + 65;
-                    mystring2 = mystring2 + String.fromCharCode(positionDecode);
+                    positionMinus = positionMinus + 65;
+                    mystring2 = mystring2 + String.fromCharCode(positionMinus);
                 }
             }
         }
 
         return mystring2
     }
-};
+}
 
-export default cipher;
+export default cipher; 
